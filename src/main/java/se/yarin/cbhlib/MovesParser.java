4b1e7815f1b0d96e8e8d419b8e85aa47f3b40a36
Jimmy Mårdell <jimmy.mardell@gmail.com>
2016-07-04 23:50:51 +0200

    Cleaned up moves parserı

--- src/main/java/se/yarin/cbhlib/MovesParser.java
+++ src/main/java/se/yarin/cbhlib/MovesParser.java
@@ -1,5 +1,6 @@
 package se.yarin.cbhlib;
 
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.yarin.chess.*;
@@ -18,27 +19,43 @@ public final class MovesParser {
     // Enabling this will integrity check the two parallel position structures after parsing every move
     static boolean INTEGRITY_CHECKS_ENABLED = false;
 
+    @AllArgsConstructor
+    private static class OpcodeRange {
+        private Piece piece;
+        private int opcode, pieceNo;
+    }
+
+    @AllArgsConstructor
+    private static class OpcodeMap {
+        private Piece piece;
+        private int ofs, pieceNo;
+    }
+
     private static final int OPCODE_NULLMOVE = 0;
-    private static final int OPCODE_KING = 1;
-    private static final int OPCODE_KING_OO = 9;
-    private static final int OPCODE_KING_OOO = 10;
-    private static final int OPCODE_QUEEN_1 = 11;
-    private static final int OPCODE_ROOK_1 = 39;
-    private static final int OPCODE_ROOK_2 = 53;
-    private static final int OPCODE_BISHOP_1 = 67;
-    private static final int OPCODE_BISHOP_2 = 81;
-    private static final int OPCODE_KNIGHT_1 = 95;
-    private static final int OPCODE_KNIGHT_2 = 103;
-    private static final int OPCODE_PAWNS = 111;
-    private static final int OPCODE_QUEEN_2 = 143;
-    private static final int OPCODE_QUEEN_3 = 171;
-    private static final int OPCODE_ROOK_3 = 199;
-    private static final int OPCODE_BISHOP_3 = 213;
-    private static final int OPCODE_KNIGHT_3 = 227;
     private static final int OPCODE_TWO_BYTES = 235;
     private static final int OPCODE_IGNORE = 236;
     private static final int OPCODE_START_VARIANT = 254;
     private static final int OPCODE_END_VARIANT = 255;
+
+    private static final OpcodeRange[] opcodeRanges = new OpcodeRange[] {
+            new OpcodeRange(Piece.KING,     1, 0),
+            new OpcodeRange(Piece.QUEEN,   11, 0),
+            new OpcodeRange(Piece.ROOK,    39, 0),
+            new OpcodeRange(Piece.ROOK,    53, 1),
+            new OpcodeRange(Piece.BISHOP,  67, 0),
+            new OpcodeRange(Piece.BISHOP,  81, 1),
+            new OpcodeRange(Piece.KNIGHT,  95, 0),
+            new OpcodeRange(Piece.KNIGHT, 103, 1),
+            new OpcodeRange(Piece.PAWN,   111, 0),
+            new OpcodeRange(Piece.QUEEN,  143, 1),
+            new OpcodeRange(Piece.QUEEN,  171, 2),
+            new OpcodeRange(Piece.ROOK,   199, 2),
+            new OpcodeRange(Piece.BISHOP, 213, 2),
+            new OpcodeRange(Piece.KNIGHT, 227, 2),
+            new OpcodeRange(Piece.NO_PIECE, OPCODE_TWO_BYTES, 0)
+    };
+
+    private static final OpcodeMap[] opcodeMap = new OpcodeMap[256];
 
     private static final int[] kingDir = new int[] { 1, 9, 8, 7, -1, -9, -8, -7};
     private static final int[] knightDir = new int[] { 17, 10, -6, -15, -17, -10, 6, 15 };
@@ -59,6 +76,15 @@ public final class MovesParser {
         } catch (IOException e) {
             throw new RuntimeException("Failed to load resource moveEncryptionKey.bin");
         }
+
+        // Map the opcode ranges into lookup tables for each opcode
+        for (int op = 1, ofs = 0, rangeNo = 0; op < 256; op++, ofs++) {
+            if (rangeNo+1 < opcodeRanges.length && opcodeRanges[rangeNo+1].opcode == op) {
+                rangeNo++;
+                ofs=0;
+            }
+            opcodeMap[op] = new OpcodeMap(opcodeRanges[rangeNo].piece, ofs, opcodeRanges[rangeNo].pieceNo);
+        }
     }
 
     public static GameMovesModel parseMoveData(ByteBuffer buf)
@@ -76,8 +102,11 @@ public final class MovesParser {
             model = new GameMovesModel();
         }
 
-        log.debug(String.format("Parsing move data at moveData pos %s with %d bytes left",
-                model.root().position().toString(), buf.limit() - buf.position()));
+        if (log.isDebugEnabled()) {
+            log.debug(String.format("Parsing move data at moveData pos %s with %d bytes left",
+                    model.root().position().toString("|"), buf.limit() - buf.position()));
+        }
+
         StonePositions piecePosition = StonePositions.fromPosition(model.root().position());
 
         int modifier = 0;
@@ -86,25 +115,25 @@ public final class MovesParser {
         GameMovesModel.Node currentNode = model.root();
 
         while (true) {
-            int data = ByteBufferUtil.getUnsignedByte(buf);
+            int opcode = ByteBufferUtil.getUnsignedByte(buf);
             if (encoded) {
-                data = decryptMap[(data + modifier) % 256];
+                opcode = decryptMap[(opcode + modifier) % 256];
             }
 
-            if (data == OPCODE_IGNORE) {
-                // Not sure what this opcode do. Just ignoring it seems works fine.
+            if (opcode == OPCODE_IGNORE) {
+                // Not sure what this opcode does. Just ignoring it seems works fine.
                 // Chessbase 9 removes this opcode when replacing the game.
                 continue;
             }
-            if (data > OPCODE_IGNORE && data < OPCODE_START_VARIANT) {
-                throw new ChessBaseInvalidDataException(String.format("Unknown opcode in game data: 0x%02X", data));
+            if (opcode > OPCODE_IGNORE && opcode < OPCODE_START_VARIANT) {
+                throw new ChessBaseInvalidDataException(String.format("Unknown opcode in game data: 0x%02X", opcode));
             }
-            if (data == OPCODE_START_VARIANT) {
+            if (opcode == OPCODE_START_VARIANT) {
                 nodeStack.push(currentNode);
                 piecePositionStack.push(piecePosition);
                 continue;
             }
-            if (data == OPCODE_END_VARIANT) {
+            if (opcode == OPCODE_END_VARIANT) {
                 // Also used as end of game
                 if (nodeStack.size() == 0)
                     break;
@@ -116,7 +145,7 @@ public final class MovesParser {
 
             // Decode the move
             Move move;
-            if (data == OPCODE_TWO_BYTES) {
+            if (opcode == OPCODE_TWO_BYTES) {
                 // In rare cases a move has to be encoded as two bytes
                 // Typically pawn promotions or if a player has more than 3 pieces of some kind
                 int msb = ByteBufferUtil.getUnsignedByte(buf);
@@ -125,10 +154,10 @@ public final class MovesParser {
                     msb = decryptMap[(msb + modifier) % 256];
                     lsb = decryptMap[(lsb + modifier) % 256];
                 }
-                int word = msb * 256 + lsb;
-                move = decodeTwoByteMove(word, currentNode.position());
+                opcode = msb * 256 + lsb;
+                move = decodeTwoByteMove(opcode, currentNode.position());
             } else {
-                move = decodeSingleByteMove(data, piecePosition, currentNode.position());
+                move = decodeSingleByteMove(opcode, piecePosition, currentNode.position());
             }
 
             if (log.isDebugEnabled()) {
@@ -149,136 +178,77 @@ public final class MovesParser {
         return model;
     }
 
-    private static Move decodeSingleByteMove(int data, StonePositions stonePositions, Position position)
+    private static Move decodeSingleByteMove(int opcode, StonePositions stonePositions, Position position)
             throws ChessBaseInvalidDataException {
         Player playerToMove = position.playerToMove();
 
-        if (data == OPCODE_NULLMOVE) {
+        if (opcode == OPCODE_NULLMOVE) {
             return Move.nullMove(position);
         }
-
-        if (data < OPCODE_QUEEN_1) {
-            // King move
-            Stone kingStone = Piece.KING.toStone(playerToMove);
-            int backRank = playerToMove == Player.WHITE ? 0 : 7;
-            if (data == OPCODE_KING_OO)
-                return new Move(position, 4, backRank, 6, backRank);
-            if (data == OPCODE_KING_OOO)
-                return new Move(position, 4, backRank, 2, backRank);
-            int dir = data - OPCODE_KING;
-            int ksqi = stonePositions.getSqi(kingStone, 0);
-            return new Move(position, ksqi, ksqi + kingDir[dir]);
+        if (opcode < OPCODE_NULLMOVE || opcode > OPCODE_TWO_BYTES) {
+            throw new ChessBaseInvalidDataException("Invalid opcode: " + opcode);
         }
 
-        // TODO: Eliminate these range checks and multitude of if-statements somehow
-        // But first add tests that uses all three pieces of each type
-        if ((data < OPCODE_KNIGHT_1) || (data >= OPCODE_QUEEN_2 && data < OPCODE_KNIGHT_3)) {
-            // Bishop, rook or queen move
-            Piece pieceType;
-            int stoneNo;
-            if (data >= OPCODE_BISHOP_3) {
-                stoneNo = 2;
-                data -= OPCODE_BISHOP_3;
-                pieceType = Piece.BISHOP;
-            } else if (data >= OPCODE_ROOK_3) {
-                stoneNo = 2;
-                data -= OPCODE_ROOK_3;
-                pieceType = Piece.ROOK;
-            } else if (data >= OPCODE_QUEEN_3) {
-                stoneNo = 2;
-                data -= OPCODE_QUEEN_3;
-                pieceType = Piece.QUEEN;
-            } else if (data >= OPCODE_QUEEN_2) {
-                stoneNo = 1;
-                data -= OPCODE_QUEEN_2;
-                pieceType = Piece.QUEEN;
-            } else if (data >= OPCODE_BISHOP_2) {
-                stoneNo = 1;
-                data -= OPCODE_BISHOP_2;
-                pieceType = Piece.BISHOP;
-            } else if (data >= OPCODE_BISHOP_1) {
-                stoneNo = 0;
-                data -= OPCODE_BISHOP_1;
-                pieceType = Piece.BISHOP;
-            } else if (data >= OPCODE_ROOK_2) {
-                stoneNo = 1;
-                data -= OPCODE_ROOK_2;
-                pieceType = Piece.ROOK;
-            } else if (data >= OPCODE_ROOK_1) {
-                stoneNo = 0;
-                data -= OPCODE_ROOK_1;
-                pieceType = Piece.ROOK;
-            } else if (data >= OPCODE_QUEEN_1) {
-                stoneNo = 0;
-                data -= OPCODE_QUEEN_1;
-                pieceType = Piece.QUEEN;
-            } else {
-                throw new ChessBaseInvalidDataException("Opcode error");
-            }
-            Stone stone = pieceType.toStone(playerToMove);
-            int sqi = stonePositions.getSqi(stone, stoneNo);
-            if (sqi < 0) {
-                throw new ChessBaseInvalidDataException(
-                        String.format("No piece coordinate for %s %s number %d", playerToMove, pieceType, stoneNo));
-            }
+        Piece piece = opcodeMap[opcode].piece;
+        int stoneNo = opcodeMap[opcode].pieceNo;
+        int ofs = opcodeMap[opcode].ofs;
+
+        if (piece == Piece.PAWN) {
+            stoneNo = ofs / 4;
+            ofs %= 4;
+        }
+
+        int sqi = stonePositions.getSqi(piece.toStone(playerToMove), stoneNo);
+        if (sqi < 0) {
+            throw new ChessBaseInvalidDataException(
+                    String.format("No piece coordinate for %s %s number %d", playerToMove, piece, stoneNo));
+        }
+
+        if (piece == Piece.KING) {
+            int backRank = playerToMove == Player.WHITE ? 0 : 7;
+            if (ofs == 8) return new Move(position, 4, backRank, 6, backRank);
+            if (ofs == 9) return new Move(position, 4, backRank, 2, backRank);
+            return new Move(position, sqi, sqi + kingDir[ofs]);
+        }
+
+        if (piece == Piece.BISHOP || piece == Piece.ROOK || piece == Piece.QUEEN) {
             int px = Chess.sqiToCol(sqi), py = Chess.sqiToRow(sqi);
-            int dir = data / 7, stride = data % 7 + 1;
-            switch (dir + (pieceType == Piece.BISHOP ? 2 : 0)) {
+            int dir = ofs / 7, stride = ofs % 7 + 1;
+            switch (dir + (piece == Piece.BISHOP ? 2 : 0)) {
                 case 0: return new Move(position, sqi, Chess.coorToSqi(px, (py + stride) % 8));
                 case 1: return new Move(position, sqi, Chess.coorToSqi((px + stride) % 8, py));
                 case 2: return new Move(position, sqi, Chess.coorToSqi((px + stride) % 8, (py + stride) % 8));
                 case 3: return new Move(position, sqi, Chess.coorToSqi((px + stride) % 8, (py + 8 - stride) % 8));
             }
-        } else if ((data < OPCODE_PAWNS) || (data >= OPCODE_KNIGHT_3 && data < OPCODE_TWO_BYTES)) {
-            int stoneNo;
-            if (data >= OPCODE_KNIGHT_3) {
-                stoneNo = 2;
-                data -= OPCODE_KNIGHT_3;
-            } else if (data >= OPCODE_KNIGHT_2) {
-                stoneNo = 1;
-                data -= OPCODE_KNIGHT_2;
-            } else {
-                stoneNo = 0;
-                data -= OPCODE_KNIGHT_1;
-            }
-            Stone stone = Piece.KNIGHT.toStone(playerToMove);
-            int sqi = stonePositions.getSqi(stone, stoneNo);
-            if (sqi < 0) {
-                throw new ChessBaseInvalidDataException(
-                        String.format("No piece coordinate for %s number %d", stone.toString(), stoneNo));
-            }
-            return new Move(position, sqi, sqi + knightDir[data]);
-        } else if (data >= OPCODE_PAWNS && data < OPCODE_QUEEN_2) {
-            data -= OPCODE_PAWNS;
-            int stoneNo = data / 4;
-            Stone stone = Piece.PAWN.toStone(playerToMove);
-            int pawnMove = data % 4;
-            int sqi = stonePositions.getSqi(stone, stoneNo);
-            if (sqi < 0) {
-                throw new ChessBaseInvalidDataException(
-                        String.format("No piece coordinate for %s number %d", stone.toString(), stoneNo));
-            }
+        }
+
+        if (piece == Piece.KNIGHT) {
+            return new Move(position, sqi, sqi + knightDir[ofs]);
+        }
+
+        if (piece == Piece.PAWN) {
             int dir = playerToMove == Player.WHITE ? 1 : -1;
-            switch (pawnMove) {
+            switch (ofs) {
                 case 0: return new Move(position, sqi, sqi + dir);
                 case 1: return new Move(position, sqi, sqi + dir * 2);
                 case 2: return new Move(position, sqi, sqi + dir * 9);
                 case 3: return new Move(position, sqi, sqi - dir * 7);
             }
         }
-        throw new ChessBaseInvalidDataException("Opcode error");
+
+        throw new ChessBaseInvalidDataException("Invalid opcode: " + opcode);
     }
 
-    private static Move decodeTwoByteMove(int data, Position board)
+    private static Move decodeTwoByteMove(int opcode, Position board)
             throws ChessBaseInvalidDataException {
         Player playerToMove = board.playerToMove();
-        int fromSqi = data % 64, toSqi = (data / 64) % 64;
-        Piece pieceType = board.stoneAt(fromSqi).toPiece();
-        if (pieceType == Piece.NO_PIECE) {
+        int fromSqi = opcode % 64, toSqi = (opcode / 64) % 64;
+        Piece piece = board.stoneAt(fromSqi).toPiece();
+        if (piece == Piece.NO_PIECE) {
             throw new ChessBaseInvalidDataException("No piece at source position");
         }
 
-        if (pieceType != Piece.PAWN) {
+        if (piece != Piece.PAWN) {
             return new Move(board, fromSqi, toSqi);
         }
 
@@ -289,7 +259,7 @@ public final class MovesParser {
         }
 
         Piece promotedPiece;
-        switch (data / 4096) {
+        switch (opcode / 4096) {
             case 0: promotedPiece = Piece.QUEEN;  break;
             case 1: promotedPiece = Piece.ROOK;   break;
             case 2: promotedPiece = Piece.BISHOP; break;
@@ -297,8 +267,8 @@ public final class MovesParser {
             default:
                 throw new ChessBaseInvalidDataException("Illegal promoted piece");
         }
-        Stone promotedStone = promotedPiece.toStone(playerToMove);
-        return new Move(board, fromSqi, toSqi, promotedStone);
+
+        return new Move(board, fromSqi, toSqi, promotedPiece.toStone(playerToMove));
     }
 
     private static GameMovesModel parseInitialPosition(ByteBuffer buf)
