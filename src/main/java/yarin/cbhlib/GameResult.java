c69ebf29f45624c63338751b0b5793d4d791e5ae
Jimmy Mårdell <jimmy.mardell@gmail.com>
2016-09-11 22:53:16 +0200

    Removed old yarin package

--- src/main/java/yarin/cbhlib/GameResult.java
+++
@@ -1,5 +0,0 @@
-package yarin.cbhlib;
-
-public enum GameResult {
-    BlackWon, Draw, WhiteWon, Line, BlackWonOnForfeit, DrawOnForfeit, WhiteWonOnForfeit, BothLost
-}
--- src/main/java/yarin/cbhlib/TournamentType.java
+++
@@ -1,9 +0,0 @@
-package yarin.cbhlib;
-
-/**
- * Created by yarin on 04/05/16.
- */
-public enum TournamentType
-{
-    Undefined, Game, Match, Tourn, Swiss, Team, KnockOut, Simul, Schev
-}
