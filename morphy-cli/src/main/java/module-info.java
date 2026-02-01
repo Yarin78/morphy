d20854e9add2d4ba1c557ea1ab1ae08159938997
Jimmy Mårdell <jimmy.mardell@gmail.com>
2026-02-01 11:59:08 +0100

    Formatting

--- morphy-cli/src/main/java/module-info.java
+++ morphy-cli/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module se.yarin.morphy.cli {
-  requires se.yarin.morphy.cbh;
-  requires info.picocli;
-  requires me.tongfei.progressbar;
-  requires org.slf4j;
-  requires ch.qos.logback.classic;
+    requires se.yarin.morphy.cbh;
+    requires info.picocli;
+    requires me.tongfei.progressbar;
+    requires org.slf4j;
+    requires ch.qos.logback.classic;
 
-  opens se.yarin.morphy.cli.commands to
-      info.picocli;
+    opens se.yarin.morphy.cli.commands to
+            info.picocli;
 }
--- morphy-tools/src/main/java/module-info.java
+++ morphy-tools/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module se.yarin.morphy.tools {
-  requires se.yarin.morphy.cbh;
-  requires org.slf4j;
-  requires java.sql;
+    requires se.yarin.morphy.cbh;
+    requires org.slf4j;
+    requires java.sql;
 }
