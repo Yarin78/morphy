e9811ddd7e479ba4607cdbfa2d1c54318ade83f7
Jimmy Mårdell <jimmy.mardell@gmail.com>
2026-02-11 23:36:05 +0100

    Fixed tests

--- morphy-service/src/test/java/se/yarin/morphy/service/MorphyServiceApplicationTest.java
+++ morphy-service/src/test/java/se/yarin/morphy/service/MorphyServiceApplicationTest.java
@@ -2,8 +2,10 @@ package se.yarin.morphy.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MorphyServiceApplicationTest {
 
   @Test
